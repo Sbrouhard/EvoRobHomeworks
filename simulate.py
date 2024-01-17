cca4d7b6f15c057699d53d7681aab81ded9d9059
Sbrouhard <spencer.brouhard@uvm.edu>
2024-01-17 12:35:13 -0500

    stepping through sim with sleep

--- simulate.py
+++ simulate.py
@@ -1,4 +1,12 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+for i in range(0, 1000):
+    print(i)
+    time.sleep(0.0066)
+    p.stepSimulation()
+
 p.disconnect()
