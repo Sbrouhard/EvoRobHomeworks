ef36ce0ed49466fd8ceca14a2ce041570d302408
Sbrouhard <spencer.brouhard@uvm.edu>
2024-02-19 17:26:44 -0500

    motor added

--- simulate.py
+++ simulate.py
@@ -25,21 +25,30 @@ planeId = p.loadURDF("plane.urdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
 
-backLegSensorValues = numpy.zeros(200)
-frontLegSensorValues = numpy.zeros(200)
 
 
-for i in range(0, 200):
+for i in range(0, 100000):
     time.sleep(0.001)
     p.stepSimulation()
-    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+
+    pyrosim.Set_Motor_For_Joint(
+    bodyIndex = robotId,
+
+    jointName = b"Torso_BackLeg",
+
+    controlMode = p.POSITION_CONTROL,
+
+    targetPosition = -3.1415926 / 4.0,
+
+    maxForce = 500)
 
 
-with open('data/backTouchSensorData.npy', 'wb') as f:
-    numpy.save(f, backLegSensorValues)\
+# with open('data/backTouchSensorData.npy', 'wb') as f:
+#     numpy.save(f, backLegSensorValues)\
 
-with open('data/frontTouchSensorData.npy', 'wb') as f:
-    numpy.save(f, frontLegSensorValues)
+# with open('data/frontTouchSensorData.npy', 'wb') as f:
+#     numpy.save(f, frontLegSensorValues)
 
 p.disconnect()
