f90aca24052b43d62abf2a6ef2e35a9fa7c94c79
Sbrouhard <spencer.brouhard@uvm.edu>
2024-03-03 19:06:48 -0500

    finished assignmet 6

--- simulate.py
+++ simulate.py
@@ -4,89 +4,60 @@ import pyrosim.pyrosim as pyrosim
 import time
 import numpy as np
 import random
+import constants as c
+from simulation import SIMULATION
 
 
 
 
 
-physicsClient = p.connect(p.GUI)
-p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+# physicsClient = p.connect(p.GUI)
+# p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 
 
-p.setGravity(0,0,-9.8)
+# p.setGravity(0,0,-9.8)
 
-p.loadSDF("world.sdf")
-
-
-robotId = p.loadURDF("body.urdf")
-planeId = p.loadURDF("plane.urdf")
+# p.loadSDF("world.sdf")
 
 
-
-pyrosim.Prepare_To_Simulate(robotId)
-
-x = np.linspace(0, 2 * np.pi, 1000)
-targetAngles = np.sin(x) * (np.pi / 4.0)
+# robotId = p.loadURDF("body.urdf")
+# planeId = p.loadURDF("plane.urdf")
 
 
-FrontLegAmplitude = np.pi/4
-FrontLegFrequency = 5
-FrontLegPhaseOffset = np.pi/4
 
-FrontLegTargetAngles = np.zeros(1000)
-for i in range(0, FrontLegTargetAngles.size):
-     FrontLegTargetAngles[i] = FrontLegAmplitude * np.sin(FrontLegFrequency * x[i] + FrontLegPhaseOffset) 
+# pyrosim.Prepare_To_Simulate(robotId)
 
-BackLegAmplitude = np.pi/4
-BackLegFrequency = 5
-BackLegPhaseOffset = 0
-
-BackLegTargetAngles = np.zeros(1000)
-for i in range(0, BackLegTargetAngles.size):
-     BackLegTargetAngles[i] = BackLegAmplitude * np.sin(BackLegFrequency * x[i] + BackLegPhaseOffset)  
+# x = np.linspace(0, 2 * np.pi, 1000)
+# targetAngles = np.sin(x) * (np.pi / 4.0)
 
 
-for i in range(0, 1000):
-    time.sleep(0.001)
-    p.stepSimulation()
-    pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-
-    pyrosim.Set_Motor_For_Joint(
-    bodyIndex = robotId,
-
-    jointName = b"Torso_BackLeg",
-
-    controlMode = p.POSITION_CONTROL,
-
-    targetPosition =  BackLegTargetAngles[i],
-
-    maxForce = 60)
-
-    pyrosim.Set_Motor_For_Joint(
-    bodyIndex = robotId,
-
-    jointName = b"Torso_FrontLeg",
-
-    controlMode = p.POSITION_CONTROL,
-
-    targetPosition = FrontLegTargetAngles[i],
-
-    maxForce = 60)
 
 
-# with open('data/backTouchSensorData.npy', 'wb') as f:
-#     numpy.save(f, backLegSensorValues)\
+# FrontLegTargetAngles = np.zeros(1000)
+# for i in range(0, FrontLegTargetAngles.size):
+#      FrontLegTargetAngles[i] = c.FrontLegAmplitude * np.sin(c.FrontLegFrequency * x[i] + c.FrontLegPhaseOffset) 
+
+# BackLegTargetAngles = np.zeros(1000)
+# for i in range(0, BackLegTargetAngles.size):
+#      BackLegTargetAngles[i] = c.BackLegAmplitude * np.sin(c.BackLegFrequency * x[i] + c.BackLegPhaseOffset)  
+
+
+
     
-with open('data/frontLegSinvalues.npy', 'wb') as f:
-     np.save(f, FrontLegTargetAngles)\
+# with open('data/frontLegSinvalues.npy', 'wb') as f:
+#      np.save(f, FrontLegTargetAngles)\
      
        
-with open('data/backLegSinvalues.npy', 'wb') as f:
-     np.save(f, BackLegTargetAngles)\
+# with open('data/backLegSinvalues.npy', 'wb') as f:
+#      np.save(f, BackLegTargetAngles)\
 
-# with open('data/frontTouchSensorData.npy', 'wb') as f:
-#     numpy.save(f, frontLegSensorValues)
+# # with open('data/frontTouchSensorData.npy', 'wb') as f:
+# #     numpy.save(f, frontLegSensorValues)
 
-p.disconnect()
+# p.disconnect()
+
+
+simulation = SIMULATION()
+simulation.Run()
